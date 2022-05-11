b021de77fe462421033799738e40009b38f87ed8
FlorNV <florencianoelivaldez@gmail.com>
2022-05-11 12:36:59 -0300

    page1 terminada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ContainerAppComponent } from './components/container-app/container-app.component';
 import { PageaComponent } from './components/pagea/pagea.component';
 import { PagebComponent } from './components/pageb/pageb.component';
 import { PagecComponent } from './components/pagec/pagec.component';
@@ -14,6 +15,7 @@ import { FooterComponent } from './components/layout/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
+    ContainerAppComponent,
     PageaComponent,
     PagebComponent,
     PagecComponent,
